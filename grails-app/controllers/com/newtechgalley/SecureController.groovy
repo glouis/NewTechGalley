e5f6308cb7055efc71f0f21735185f4ad8f48957
glouis <louis.gourdeau@hotmail.fr>
2015-01-21 16:16:37 +0100

    add @secured

--- grails-app/controllers/com/newtechgalley/SecureController.groovy
+++ grails-app/controllers/com/newtechgalley/SecureController.groovy
@@ -2,9 +2,9 @@ package com.newtechgalley
 
 import org.springframework.security.access.annotation.Secured
 
+@Secured(['ROLE_ADMIN'])
 class SecureController {
 
-    @Secured(['ROLE_ADMIN'])
     def index() {
         render 'Secure access only'
     }
--- grails-app/controllers/com/newtechgalley/WelcomeController.groovy
+++ grails-app/controllers/com/newtechgalley/WelcomeController.groovy
@@ -1,5 +1,8 @@
 package com.newtechgalley
 
+import org.springframework.security.access.annotation.Secured
+
+@Secured(['ROLE_ADMIN'])
 class WelcomeController {
 
     def index() {
